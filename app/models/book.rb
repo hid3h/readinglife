afd1746d74bcc6230a6fd59a3bdec9b3aa1b90d4
hide <razyetts@hotmail.co.jp>
2020-11-08 15:48:55 +0900

    本とリンクを保存

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,25 +5,27 @@ class Book < ApplicationRecord
     def create_by_rakuten_search_result(books)
       isbn_list = books.map { |book| book['isbn'] }
       exist_book_isbn_list = Book.where(isbn: isbn_list).pluck(:isbn)
-      p "existed_book_isbn_list", exist_book_isbn_list
 
       new_isbn_list = isbn_list - exist_book_isbn_list
 
       new_isbn_list.each do |isbn|
         book = books.find { |b| b['isbn'] == isbn }
-        result = create(
-          isbn:           book['isbn'],
-          title:          book['title'],
-          image_url:      book['largeImageUrl'],
-          author:         book['author'],
-          caption:        book['itemCaption'],
-          publisher_name: book['publisherName']
-        )
 
-        result.book_links.create(
-          site: BookLink.sites[:rakuten],
-          url:  book['itemUrl']
-        )
+        transaction do
+          result = create(
+            isbn:           book['isbn'],
+            title:          book['title'],
+            image_url:      book['largeImageUrl'],
+            author:         book['author'],
+            caption:        book['itemCaption'],
+            publisher_name: book['publisherName']
+          )
+
+          result.book_links.create(
+            site: BookLink.sites[:rakuten],
+            url:  book['itemUrl']
+          )
+        end
       end
 
       Book.where(isbn: isbn_list).includes(:book_links)

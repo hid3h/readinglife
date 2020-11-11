5074fa7f63b643d6f1dd6018969c091736ff4c4a
hidetaka hasegawa <>
2020-11-11 20:52:41 +0900

    不要削除とログ

--- app/models/graph_replyer.rb
+++ app/models/graph_replyer.rb
@@ -13,7 +13,6 @@ class GraphReplyer
     # 月ごとのカウント
     # TODO 年対応
     count_by_monthly = user.bookshelves.read.group("MONTH(created_at)").count
-    p "count_by_monthly", count_by_monthly.fetch(11, 'Not found')
     labels = %w(1月 2月 3月 4月 5月 6月 7月 8月 9月 10月 11月 12月)
     data = labels.map do |m|
       i = m.delete('月').to_i
@@ -35,72 +34,8 @@ class GraphReplyer
       reply_token: @line_event.reply_token,
       message: message_hash
     )
-    p "res", res
-  end
-
-  private
-
-  def book_id
-    postback_params['book_id']
-  end
-
-  def bookshelf_id
-    postback_params['bookshelf_id']
-  end
-
-  def action_type
-    postback_params['action']
-  end
-
-  def postback_params
-    Rack::Utils.parse_nested_query(@line_event.postback_data)
-  end
-
-  def make_template_message(books)
-    # 最大10個しかかえせない
-    books = books.slice(0, 9)
-    columns = books.map.with_index { |book, index|
-      {
-        thumbnailImageUrl: book.image_url,
-        imageBackgroundColor: "#FFFFFF", # デフォルト
-        # title: item["title"], # 任意
-        text: book.title, # 必須
-        # defaultAction: {
-        #   type: "uri",
-        #   label: "View detail",
-        #   uri: "http://example.com/page/123"
-        # },
-        actions: [
-          {
-              type: "postback",
-              label: "読んだ",
-              data: "action=read&book_id=#{book.id}"
-          },
-          {
-              type: "postback",
-              label: "読みたい",
-              data: "action=want_to_read&book_id=#{book.id}"
-          },
-          {
-              type: "uri",
-              label: "楽天リンク",
-              uri: book.book_links.first.url # rakutneしかない
-          }
-        ]
-      }
-    }
-
-    template = {
-      type: "carousel",
-      imageAspectRatio: "square",
-      imageSize: "contain",
-      columns: columns
-    }
-    message_hash = {
-      type: 'template',
-      altText: "this is a carousel template",
-      template: template
-    }
+    p "res", res.body
+    p "message_hash", message_hash
   end
 
   # TODO privateメソッドで生やすよりいい置き場所ありそうな気がします
